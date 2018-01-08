8c24fc73a991fcce7ef7d983dabda03d9b52a6d0
Binh Nguyen <bsflash2014@gmail.com>
2018-01-07 20:40:54 -0800

    prep repo for heroku

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express = require('express')
 
 // Create out app
 var app = express();
+//Heroku
+const PORT = process.env.PORT || 3001;
 
+app.use(function(req, res, next){
+  if (req.headers['x-forwarded-proto']==='http'){
+    next();
+  }else{
+    res.direct('http://'+req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
-app.listen(3001,function (){
-  console.log('Express is up on port 3001');
+
+app.listen(PORT,function (){
+  console.log('Express is up on port ' + PORT);
 });
